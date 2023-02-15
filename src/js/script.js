c5d77fc538513aad6c2f2be3fc23696fc7dcd0f4
garted <garted93@gmail.com>
2023-02-15 17:56:32 +0300

    maponfooter

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,14 @@
 $(document).ready(function () {
   $(".carousel__inner").slick({
-    speed: 1000,
-    fade: true,
-    cssEase: "linear",
-    autoplay: true,
-    autoplaySpeed: 3000,
-    fade: true,
-    cssEase: "linear",
+    speed: 1200,
+    adaptiveHeight: true,
     prevArrow:
-      '<button type="button" class="slick-prev"><img src="../img/ico/prev.png" alt="lft"></button>',
+      '<button type="button" class="slick-prev"><img src="../icos/next.svg"></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><img src="../img/ico/next.png" alt="lft"></button>',
+      '<button type="button" class="slick-next"><img src="../icos/prev.svg"></button>',
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 992,
         settings: {
           dots: true,
           arrows: false,
@@ -21,4 +16,35 @@ $(document).ready(function () {
       },
     ],
   });
+
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+      });
+    });
+  }
+
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
 });
